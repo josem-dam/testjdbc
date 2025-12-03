70b02dc7d0e2c7d0e4fcad202ff6d531bf50a6aa
José Miguel SA <jsanale860 @g.educaand.es>
2025-12-03 11:19:31 +0100

    Añade clases DAO

---
+++ src/main/java/edu/acceso/testjdbc/dao/CentroDao.java
@@ -0,0 +1,122 @@
+package edu.acceso.testjdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.acceso.testjdbc.ConnectionPool;
+import edu.acceso.testjdbc.domain.Centro;
+import edu.acceso.testjdbc.domain.Titularidad;
+
+public class CentroDao implements GenericDao<Centro> {
+
+    private ConnectionPool cp;
+
+    public CentroDao(ConnectionPool cp) {
+        this.cp = cp;
+    }
+
+    private static Centro resultSetToCentro(ResultSet rs) throws SQLException {
+        int id = rs.getInt("id");
+        String nombre = rs.getString("nombre");
+        Titularidad titularidad = Titularidad.fromString(rs.getString("titularidad"));
+        return new Centro(id, nombre, titularidad);
+    }
+
+    @Override
+    public Centro get(int id) throws SQLException {
+        String sqlString = "SELECT * FROM Centro WHERE id = ?";
+
+        try(
+            Connection conn = cp.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sqlString);
+        ) {
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next() ? resultSetToCentro(rs) : null;
+        }
+    }
+
+    @Override
+    public List<Centro> get() throws SQLException {
+        String sqlString = "SELECT * FROM Centro";
+
+        List<Centro> centros = new ArrayList<>();
+
+        try(
+            Connection conn = cp.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sqlString);
+        ) {
+            // ¡¡ Cuidado con que falle la creación de un centro !!
+            while(rs.next()) {
+                try {
+                    Centro centro = resultSetToCentro(rs);
+                    centros.add(centro);
+                } catch(SQLException e) {
+                    System.err.println("Un registro no puede convertirse en centro: " + e.getMessage());
+                }
+            }
+        }
+
+        return centros;
+    }
+
+    @Override
+    public boolean remove(int id) throws SQLException {
+        String sqlString = "DELETE FROM Centro WHERE id = ?";
+
+        try(
+            Connection conn = cp.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sqlString);
+        ) {
+            pstmt.setInt(1, id);
+            int rows = pstmt.executeUpdate();
+            return rows > 0;
+        }
+    }
+
+    private void setParams(PreparedStatement pstmt, Centro centro) throws SQLException {
+        pstmt.setString(1, centro.getNombre());
+        pstmt.setString(2, centro.getTitularidad().toString());
+        pstmt.setInt(3, centro.getId());
+    }
+
+    @Override
+    public int insert(Centro centro) throws SQLException {
+        String sqlString = "INSERT INTO Centro (nombre, titularidad, id) VALUES (?, ?, ?)";
+
+        try(
+            Connection conn = cp.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sqlString);
+        ) {
+            setParams(pstmt, centro);
+            pstmt.executeUpdate();
+            return centro.getId();
+        }
+    }
+
+    @Override
+    public void insert(Iterable<Centro> entities) throws SQLException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Centro centro) throws SQLException {
+        String sqlString = "UPDATE Centro SET nombre = ?, titularidad = ? WHERE id = ?";
+
+        try(
+            Connection conn = cp.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sqlString);
+        ) {
+            setParams(pstmt, centro);
+            int rows = pstmt.executeUpdate();
+            if(rows == 0) throw new IllegalArgumentException(String.format("El centro con ID %d no existe", centro.getId()));
+        }
+    }
+}
